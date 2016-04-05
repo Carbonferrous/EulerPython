0ca318acaa07d53aea2acad1ec4cd62cd3fdfa5b
Carbon Ferrous <Carbonferrous@users.noreply.github.com>
2016-04-05 07:10:47 -0500

    Update p080.py

--- p080.py
+++ p080.py
@@ -1,3 +1,3 @@
-import numbertheory
+import numbertheory, itertools
 
-print(sum(sum(int(x) for x in str(numbertheory.stringSqrt(x, 100))) for x in range(1, 101)) - sum(x for x in range(1, 10)) - 1)
+print(sum(sum(itertools.islice(numbertheory.sqrtGen(x),100))) for x in range(1, 101)) - sum(x for x in range(1, 10)) - 1)
