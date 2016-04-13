8462977c120d31c4b0c54d418124897cfba65918
Carbon Ferrous <carbonferrous@gmail.com>
2016-04-13 10:46:12 -0500

    94 failure

--- p094.py
+++ p094.py
@@ -1,9 +1,22 @@
+import numbertheory
+
 def area(a, b, c):
     s = (a + b + c)/2
     return (s*(s-a)*(s-b)*(s-c))**.5
 
-def isSqr(x):
-    est = int(x**.5)
-    return est**2 == x
-
-print(area(5,5,6))
+s = 0
+for x in range(2, 10**6):
+    if area(x, x, x - 1).is_integer():
+        s += 3* x - 1
+        print(x, x, x - 1, "M")
+    if area(x, x, x + 1).is_integer():
+        s += 3 * x + 1
+        print(x, x, x + 1, "P")
+print("Sum:", s)
+a = numbertheory.contfracsqrt(3)
+b = [next(a), next(a), next(a)]
+for x in range(10):
+    print(numbertheory.contfrac2real(b))
+    b += [b[1]]
+    print(numbertheory.contfrac2real(b))
+    b += [b[2]]
