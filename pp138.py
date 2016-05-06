3dd75c216f09e1ea705bf8fdefa90dc9c30d327d
carbonferrous <carbonferrous@gmail.com>
2016-05-06 17:44:02 -0600

    fix

--- pp138.py
+++ pp138.py
@@ -11,17 +11,13 @@ def fib(n):
         else:
             return (d, c + d)
 
-def sol(n):
+def sol1(n):
     return sum(fib(n)[0] for n in range(9, 6*n+3 + 1, 6))>>1
 
 #f(6n+3) = 5f(6(n-1)+3)+8f(6(n-1)+3+1)
 #f(6n+3+1) = 8f(6(n-1)+3)+13f(6(n-1)+3+1)
 #f(6+3)+f(6*2+3)+...+f(6*(n-1)+3)+f(6n+3)
-<<<<<<< HEAD
-def sol2(n):
-=======
 def sol2(n, x):
->>>>>>> origin/master
     a = 17
     b = 55
     s = 0
@@ -31,7 +27,6 @@ def sol2(n, x):
         a, b = a, b
     return s
 
-<<<<<<< HEAD
 def sol3(n, x=10**9+7):
     m = n
     y = x
@@ -44,18 +39,15 @@ def sol3(n, x=10**9+7):
         s += a
     return s%y
 
-print(sol3(1234567, 10**9+7))
-=======
 #calculates solution directly
 def sol4(n):
-    a = 17
-    b = 322
-    for i in range(n-1):
+    a = 0
+    b = 17
+    for i in range(n):
         a, b = b, 18*b-a+16
     return a
 
-
->>>>>>> origin/master
+#print(sol4(1234567, 10**9+7))
 
 #for i in range(int(input())):
 #    print(sol(int(input())))
