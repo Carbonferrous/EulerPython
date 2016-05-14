ead193f054986db5aa7d5d07d59df498a7ddd6c0
carbonferrous <carbonferrous@gmail.com>
2016-05-13 18:06:37 -0600

    yay

--- p086.py
+++ p086.py
@@ -10,35 +10,25 @@ def pythagTreeTraverse(traversal):
         m, n = uad[t](m, n)
     return (m**2-n**2, 2*m*n, m**2+n**2)
 
-def pythagTree(limit = None):
-    l = limit
-    uad = {'u':lambda m, n: (2*m-n, m),
-           'a':lambda m, n: (2*m+n, m),
-           'd':lambda m, n: (m+2*n, n)}
+def pythag(limits = lambda m,n: True):
+    #a, b, c = m**2-n**2, 2*m*n, m**2+n**2
+    uad = {'u':lambda m, n: (2*m-n, m), #increases at rate approaching 1 (smaller than d)
+           'a':lambda m, n: (2*m+n, m), #increases at rate approaching 2sqrt2+3
+           'd':lambda m, n: (m+2*n, n)} #increases at rate approaching 1
+    l = limits #function of m,n to define when to continue
     root = [(2, 1)]
     branch = []
-    if l != None:
-        while len(root) > 0:
-            for m, n in root:
-                yield (m**2-n**2, 2*m*n, m**2+n**2)
-                p, q = uad['u'](m, n)
-                if p**2+q**2 < l:
-                    branch += [(p, q)]
-                p, q = uad['a'](m, n)
-                if p**2+q**2 < l:
-                    branch += [(p, q)]
-                p, q = uad['d'](m, n)
-                if p**2+q**2 < l:
-                    branch += [(p, q)]
-            root = branch
-            branch = []
-        return
-    else:
-        while True:
-            for m, n in root:
-                yield (m**2-n**2, 2*m*n, m**2+n**2)
-                branch += [uad['u'](m, n)]
-                branch += [uad['a'](m, n)]
-                branch += [uad['d'](m, n)]
-            root = branch
-            branch = []
+    while len(root) > 0:
+        for m, n in root:
+            yield (m**2-n**2, 2*m*n, m**2+n**2)
+            p, q = uad['u'](m, n)
+            if l(p, q):
+                branch += [(p, q)]
+            p, q = uad['d'](m, n)
+            if l(p, q):
+                branch += [(p, q)]
+            p, q = uad['a'](m, n)
+            if l(p, q):
+                branch += [(p, q)]
+        root = branch
+        branch = []
