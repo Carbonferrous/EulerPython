38fee428bddbb162a065b47b2a3921e00e4049ef
Carbon Ferrous <carbonferrous@gmail.com>
2016-05-17 13:58:42 -0500

    pythag triplet

--- p139.py
+++ p139.py
@@ -1,17 +1,11 @@
-from fractions import gcd
-def pyth(n, m):
-    return(n**2 - m**2, 2*n*m, n**2 + m**2)
-#sum(pyth(n, m))
-#2n**2+2nm = 2n(n+m)
-count = 0
-for n in range(1, 7072):
-    for m in range(1, n):
-        if n**2+n*m > 50000000:
-            break
-        if gcd(n, m) != 1 or (n**2+m**2) % abs(n**2-m**2-2*n*m) != 0:
-            continue
-        count += 50000000//(n**2+m*n)
-        if 50000000 % (n**2+m*n) == 0:
-            count -= 1
-            print(n, m)
-print(count)
+def sol(p):
+    count = 0
+    a, b, c = 3, 4, 5
+    i = 0
+    while a+b+c < p:
+        count += (p-1)//(a+b+c)
+        i += 1
+        a, b, c = a+2*b+2*c, 2*a+b+2*c, 2*a+2*b+3*c
+    return count
+
+print(sol(10**8))
