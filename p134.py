416fda64744ba59661bd3f385687218114d17972
Carbon Ferrous <carbonferrous@gmail.com>
2016-05-17 08:50:17 -0500

    stuff

--- p134.py
+++ p134.py
@@ -1,15 +1,16 @@
 import numbertheory
 
-primes = numbertheory.primeList(10**6+4)
+def n(p1, p2):
+    size = len(str(p1))
+    k = ((p2-p1)*pow(10**size, p2 - 2, p2))%p2
+    return k*10**size+p1
+
+primes = numbertheory.primeList(10**9)
 next(primes)#2
 next(primes)#3
 p1 = next(primes)#5
 total = 0
 for p2 in primes:
-    size = len(str(p1))
-    k = 1
-    while (k*10**size+p1) % p2 != 0:
-        k += 1
-    total += k*10**size+p1
+    total += n(p1, p2)
     p1 = p2
 print(total)
